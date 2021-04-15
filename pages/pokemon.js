330d72e8876289604236cd93ce7ffd20f7c0e920
Pravata92 <pravata.juan@gmail.com>
2021-04-14 21:34:20 -0300

    final edition, no warnings in console

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -11,7 +11,7 @@ export default function Detail({pokemon, id}){
     }, [Id])
 
 
-    return <Layout className="" title={pokemon.name }>
+    return <Layout title={pokemon.name }>
         <div className="arrow-container">
         <Link href={`/pokemon?id=${parseInt(Id) > 1 ? parseInt(Id) - 1 : 151}`}>
         <a>
@@ -37,17 +37,18 @@ export default function Detail({pokemon, id}){
                 />
                 <div className="inline-flex">
                     <div className="pr-2 pl-0 ms:pr-2 pl-0 md:pr-20 pl-5 lg:pr-20 pl-5">
-                        <h2 className="font-bold mr-2">Weight:</h2>
+                        <h2 className="font-bold mr-2 pb-2">Weight:</h2>
                             <span>{pokemon.weight}</span>
-                        <h2 className="font-bold mr-2">Height:</h2>
+                        <h2 className="font-bold mr-2 pb-2 pt-1">Height:</h2>
                             <span>{pokemon.height}</span>      
                     </div>
                     <div className="mr-5">
-                        <h2 className="font-bold mr-2">Types:</h2>
+                        <h2 className="font-bold mr-2 pb-1">Types:</h2>
                         {pokemon.types.map((type, index) => {
-                            return <span key={index}>{type.type.name}</span>
+                            return <span  key={index}>{type.type.name}<br></br></span>
+                                    
                         })}
-                        <h2 className="font-bold mr-2">Abilities:</h2>
+                        <h2 className="font-bold mr-2 pb-1 pt-1">Abilities:</h2>
                         {pokemon.abilities.map((ab, index) => {
                             return <p key={index}>{ab.ability.name}</p>
                         })}
