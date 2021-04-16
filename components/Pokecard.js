28a607546fc60cc7abf742c2aad26839295d2d76
Pravata92 <pravata.juan@gmail.com>
2021-04-16 16:36:07 -0300

    commit

--- components/Pokecard.js
+++ components/Pokecard.js
@@ -5,13 +5,13 @@ const PokeCard = ({name, img, paddedIndex, id}) =>{
       <div className="rounded-lg grow shadow-5 box-content mx-auto bg-gray-100  mb-10  shadow-lg max-w-screen-md" >
         <Link href={`/pokemon?id=${id }`}>
           <img className="flex-wrap mx-auto" src={img} style={{width:'250px', cursor: "pointer"}} alt="Pokemon" />
+        </Link>
         <div className="px-6 py-4 bg-gray-800 flex-row text-center">
           <div className="font-bold text-white text-xl mb-2 capitalize">
               {name}
           </div>
           <p className="text-white font text-base">#{paddedIndex}</p>
       </div>
-        </Link>
     </div>
     )
   }
